58df98183dceeada7e73d1fd9c9166789bed6058
hocinebouarara <hocinbouarara08@gmail.com>
2023-05-03 19:42:03 +0100

    create two instances from TextBox class

--- src/com/hocinebouarara/Main.java
+++ src/com/hocinebouarara/Main.java
@@ -1,8 +1,19 @@
 package com.hocinebouarara;
 
+import java.util.Locale;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        TextBox textBox1 = new TextBox();
+        textBox1.setText("fist name textBox");
+        System.out.println(textBox1.text.toUpperCase());
+
+        TextBox textBox2 = new TextBox();
+        textBox2.setText("last name textBox");
+
+
+
     }
 }

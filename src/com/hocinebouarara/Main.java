e5eeae62053d00330f15cc4cc8725917dce6b07c
hocinebouarara <hocinbouarara08@gmail.com>
2023-05-04 00:20:21 +0100

    this statements means "Encapsulation"

--- src/com/hocinebouarara/Main.java
+++ src/com/hocinebouarara/Main.java
@@ -6,12 +6,20 @@ public class Main {
 
     public static void main(String[] args) {
 
-        TextBox textBox1 = new TextBox();
-        textBox1.setText("fist name textBox");
-        System.out.println(textBox1.text.toUpperCase());
+//        TextBox textBox1 = new TextBox();
+//        textBox1.setText("fist name textBox");
+//        System.out.println(textBox1.text.toUpperCase());
+//
+//        TextBox textBox2 = new TextBox();
+//        textBox2.setText("last name textBox");
 
-        TextBox textBox2 = new TextBox();
-        textBox2.setText("last name textBox");
+        Employee employee = new  Employee();
+        employee.baseSalary = 45_000;
+        employee.hourlyRate = 20;
+        int wage = employee.calculateWage(10);
+
+        System.out.println(wage);
+
 
 
 

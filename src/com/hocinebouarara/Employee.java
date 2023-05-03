dde7a13d1706a5f844f62e903a1f58baebc43740
hocinebouarara <hocinbouarara08@gmail.com>
2023-05-04 00:35:18 +0100

    added the Getter and Setter

--- src/com/hocinebouarara/Employee.java
+++ src/com/hocinebouarara/Employee.java
@@ -1,10 +1,28 @@
 package com.hocinebouarara;
 
 public class Employee {
-    public int baseSalary;
-    public int hourlyRate;
+    private int baseSalary;
+    private int hourlyRate;
+
+    public void setBaseSalary(int baseSalary){
+        if (baseSalary<= 0)
+            throw new  IllegalArgumentException("Salary cannot be 0 or less.");
+        this.baseSalary =baseSalary;
+    }
+
+    public int getBaseSalary(){
+        return baseSalary;
+    }
 
     public int calculateWage(int extraHours){
-        return baseSalary + (hourlyRate * extraHours);
+        return baseSalary + (getHourlyRate() * extraHours);
+    }
+
+    public int getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public void setHourlyRate(int hourlyRate) {
+        this.hourlyRate = hourlyRate;
     }
 }

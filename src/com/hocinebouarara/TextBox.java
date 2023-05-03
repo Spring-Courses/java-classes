1716de3b5336b2f38389817f2b398e61a9f9b0e3
hocinebouarara <hocinbouarara08@gmail.com>
2023-05-03 19:36:10 +0100

    create the TextBox class

---
+++ src/com/hocinebouarara/TextBox.java
@@ -0,0 +1,16 @@
+package com.hocinebouarara;
+
+public class TextBox {
+
+    public String text;  // field
+
+    public void setText(String text){
+        this.text = text;
+    }
+
+    public void clear(){
+        this.text = "";
+    }
+
+
+}
